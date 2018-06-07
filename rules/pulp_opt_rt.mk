05668c8d0b7e09a177eabf58cff43db5cc070316
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-06-07 13:07:54 +0200

    Added option for setting runtime mode

--- rules/pulp_opt_rt.mk
+++ rules/pulp_opt_rt.mk
@@ -2,8 +2,26 @@ ifdef rt/no-werror
 override CONFIG_OPT += rt/werror=false
 endif
 
+ifdef rt/mode
+ifeq '$(rt/mode)' 'bare'
+override CONFIG_OPT += rt/mode=rtbare
+else
+ifeq '$(rt/mode)' 'tiny'
+override CONFIG_OPT += rt/mode=rttiny
+else
+ifeq '$(rt/mode)' 'pulpos'
+override CONFIG_OPT += rt/mode=rt
+else
+$(error Unknown runtime mode: $(rt/mode))
+endif
+endif
+endif
+endif
+
+
 help_opt_rt:
 	@echo
 	@echo "Available make options for runtime:"
 	@echo "  rt/no-werror=1         Deactivate errors on warnings"
+	@echo "  rt/mode=<mode>         Specify runtime mode (pulpos, tiny or bare)"
 	
