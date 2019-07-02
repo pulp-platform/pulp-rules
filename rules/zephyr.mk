88a5e84123fb7b7e0547d5ce5b643158f5eed460
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-07-02 10:50:26 +0200

    Fixed regression

--- rules/zephyr.mk
+++ rules/zephyr.mk
@@ -2,7 +2,7 @@ ifdef runner_args
 export RUNNER_ARGS=$(runner_args)
 endif
 
-ZEPHYR_SRCS = $(foreach dir,$(SRCS),$(CURDIR)/$(dir))
+ZEPHYR_SRCS = $(foreach dir,$(APP_SRCS),$(CURDIR)/$(dir))
 
 BUILDDIR = build/zephyr$(build_dir_ext)
 
@@ -12,7 +12,7 @@ endif
 
 $(BUILDDIR):
 	mkdir -p $(BUILDDIR)
-	cd $(BUILDDIR) && cmake -DBOARD=gapuino $(PULP_SDK_HOME)/install/rules/zephyr -DCMAKE_C_FLAGS="$(CFLAGS)" -DTEST_SRCS=$(ZEPHYR_SRCS) $(ZEPHYR_OPT)
+	cd $(BUILDDIR) && cmake -DBOARD=gapuino $(PULP_SDK_HOME)/install/rules/zephyr -DCMAKE_C_FLAGS="$(APP_CFLAGS)" -DTEST_SRCS=$(ZEPHYR_SRCS) $(ZEPHYR_OPT)
 
 clean:
 	rm -rf $(BUILDDIR)
